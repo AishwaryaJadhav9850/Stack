9f3bfa95162e13c669f1d6d71d37ff1d396d3203
AishwaryaJadhav9850 <39164674+AishwaryaJadhav9850@users.noreply.github.com>
2020-07-30 00:05:35 +0530

    stack files

---
+++ Stack.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 23:31:15 2020
+
+@author: SUPERNOVA2813
+"""
+
+class Stack:
+    def __init__(self):
+        self.stack=[]
+        self.top=-1
+    
+    def push(self):
+        i=int(input("Enter the element to push onto Stack:-"))
+        self.stack.append(i)
+        self.top=self.top+1
+        
+    def display(self):
+        if(self.top<0):
+            print("Stack is empty!")
+            return 
+        print("\n")
+        for i in self.stack:
+            print(i ,end="-->")
+    
+    def peek(self):
+        if(self.top<0):
+            print("Stack is empty!")
+            return
+        print("Top of the stack is :",self.stack[self.top])   
+    
+    def empty(self):
+        if(self.top<0):
+            print("Stack is empty!")
+            return
+        print("Stack has ",self.top+1," elements")
+    
+    def pop(self):
+       if(self.top<0):
+            print("Stack is empty!")
+            return
+       self.top=self.top-1 
+       print(self.stack.pop())
+
+s=Stack()
+while(1):    
+    print("\n1. Push")
+    print("2. Pop")
+    print("3. Display")
+    print("4. Top")
+    print("5. isEmpty")
+    print("6. EXIT")
+    ch=int(input("Enter your choice:-"))
+    if ch==1:
+        s.push()
+        s.display()
+    elif ch==2:
+        s.pop()
+        s.display()
+    elif ch==3:
+        s.display()
+    elif ch==4:
+        s.peek()
+    elif ch==5:
+        s.empty()
+    elif ch==6:
+        break
+    else:
+        print("Please enter correct choice!")
+    
